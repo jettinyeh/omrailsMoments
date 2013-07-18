09affe7278985627083fdadbb3c359b3c5783b8c
Jay Yeh <jyeh@maxmeanmin.com>
2013-07-18 01:15:54 -0700

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 OmrailsMoments::Application.routes.draw do
+  # setting root to home.  not get pages/home (get "pages/home")
+  get "about"=> 'pages#about'
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
